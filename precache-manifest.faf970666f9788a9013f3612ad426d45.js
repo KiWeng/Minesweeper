6ac288fe8eefbe497e3d380bad6a998fb5ba3431
Zihan Weng <wzh.k@outlook.com>
2020-10-10 22:27:42 +0800

    Second Commit

---
+++ precache-manifest.faf970666f9788a9013f3612ad426d45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+    {
+        "revision": "5dd40ceedbf8633fb6eb55caf8b18266",
+        "url": "/Minesweeper/index.html"
+    },
+    {
+        "revision": "ad5bb6d9ebb01e19bfb6",
+        "url": "/Minesweeper/static/css/main.b418f8d7.chunk.css"
+    },
+    {
+        "revision": "8c0c705d057a49ce1896",
+        "url": "/Minesweeper/static/js/2.46c573c8.chunk.js"
+    },
+    {
+        "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+        "url": "/Minesweeper/static/js/2.46c573c8.chunk.js.LICENSE.txt"
+    },
+    {
+        "revision": "ad5bb6d9ebb01e19bfb6",
+        "url": "/Minesweeper/static/js/main.979cd1a4.chunk.js"
+    },
+    {
+        "revision": "8ee82daadaa811908a44",
+        "url": "/Minesweeper/static/js/runtime-main.8b4de241.js"
+    },
+    {
+        "revision": "6153acdb0a75fa92eeca87caa4b7f6ba",
+        "url": "/Minesweeper/static/media/bomb.6153acdb.svg"
+    }
+]);
