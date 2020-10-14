95a556d1c2068d509ecfaf7deda04da62c0d49ba
Zihan Weng <wzh.k@outlook.com>
2020-10-14 21:59:45 +0800

    Made it actually playable.

---
+++ build/precache-manifest.41f9f50b352d3fa8176deb58a6904461.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+    {
+        "revision": "514abede46de9256367402c93d944537",
+        "url": "./index.html"
+    },
+    {
+        "revision": "a790ec27b9e4d40452fe",
+        "url": "./static/css/main.55bccae4.chunk.css"
+    },
+    {
+        "revision": "f2e4b0dcca5ca74d0e21",
+        "url": "./static/js/2.9eddb3b3.chunk.js"
+    },
+    {
+        "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+        "url": "./static/js/2.9eddb3b3.chunk.js.LICENSE.txt"
+    },
+    {
+        "revision": "a790ec27b9e4d40452fe",
+        "url": "./static/js/main.449db7a7.chunk.js"
+    },
+    {
+        "revision": "ca8051aa2fe5b3ef84c3",
+        "url": "./static/js/runtime-main.673723db.js"
+    },
+    {
+        "revision": "6153acdb0a75fa92eeca87caa4b7f6ba",
+        "url": "./static/media/bomb.6153acdb.svg"
+    },
+    {
+        "revision": "2e7faa2e11986f81fa08782bb735e159",
+        "url": "./static/media/fired.2e7faa2e.svg"
+    },
+    {
+        "revision": "2fd6351f1dacb23b4dfee14451b0545e",
+        "url": "./static/media/target.2fd6351f.svg"
+    }
+]);
