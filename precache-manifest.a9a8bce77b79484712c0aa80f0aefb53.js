57815c2b00150911f009e355320a1b010a9a2c71
Zihan Weng <wzh.k@outlook.com>
2020-10-14 18:31:38 +0800

    Completion of stage 3, events are handled in a more proper way.

---
+++ precache-manifest.a9a8bce77b79484712c0aa80f0aefb53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+    {
+        "revision": "44a73bd1ddaab754fa5a7da7036a3590",
+        "url": "./index.html"
+    },
+    {
+        "revision": "b385c45dbfb01e290c14",
+        "url": "./static/css/main.8ef11650.chunk.css"
+    },
+    {
+        "revision": "3480b9c4919b33eec451",
+        "url": "./static/js/2.f729e386.chunk.js"
+    },
+    {
+        "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+        "url": "./static/js/2.f729e386.chunk.js.LICENSE.txt"
+    },
+    {
+        "revision": "b385c45dbfb01e290c14",
+        "url": "./static/js/main.c316cdd7.chunk.js"
+    },
+    {
+        "revision": "ca8051aa2fe5b3ef84c3",
+        "url": "./static/js/runtime-main.673723db.js"
+    },
+    {
+        "revision": "6153acdb0a75fa92eeca87caa4b7f6ba",
+        "url": "./static/media/bomb.6153acdb.svg"
+    }
+]);
